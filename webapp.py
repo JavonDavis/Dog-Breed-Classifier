3a413b3bcb32d98d6b480a82b36dd4560db8d91b
Javon Davis <javonldavis14@gmail.com>
2017-12-29 12:54:26 -0500

    file now being uploaded and saved to folder in webapp

--- webapp.py
+++ webapp.py
@@ -1,29 +1,36 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, send_from_directory, url_for
 from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
+app.debug = True
 
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
-app.config['UPLOAD_FOLDER'] = 'uploads'
-app.config['MAX_CONTENT_PATH'] = 50 * 1024 * 24 # 50MB
+app.secret_key = 'doggyclassy'
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['UPLOAD_FOLDER'] = '/Users/javon/Projects/Dog-Breed-Classifier/uploads'
+app.config['MAX_CONTENT_PATH'] = 50 * 1024 * 24  # 50MB
+
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/home')
+
+@app.route('/home', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
         # check if the post request has the file part
-        if 'file' not in request.files:
+        if 'image_file' not in request.files:
             flash('No file part')
             return redirect(request.url)
-        file = request.files['file']
+        file = request.files['image_file']
         # if user does not select file, browser also
         # submit a empty part without filename
         if file.filename == '':
@@ -36,7 +43,7 @@ def home():
                                     filename=filename))
     return render_template('main.html')
 
+
 @app.route('/results/<filename>')
 def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename)	
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
