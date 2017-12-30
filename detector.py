23e3dceacf28e0586a62848c558fda73c44865a2
Javon Davis <javonldavis14@gmail.com>
2017-12-29 20:55:22 -0500

    removed SVM used to detect faces to allow webapp to run on non-gpu instaces

--- detector.py
+++ detector.py
@@ -5,6 +5,7 @@ from keras.models import model_from_json
 from keras.preprocessing import image
 from sklearn.externals import joblib
 from tqdm import tqdm
+import cv2
 
 from extract_bottleneck_features import extract_InceptionV3
 
@@ -17,13 +18,19 @@ dog_breed_model = model_from_json(loaded_model_json)
 dog_breed_model.load_weights("models/model-weights.h5")
 print("Loaded model from disk")
 
-clf = joblib.load('models/face_classifier.pkl')
-print("Loaded classifier from disk")
+# Causes Memory Error on non-GPU instance
+# clf = joblib.load('models/face_classifier.pkl')
+# print("Loaded classifier from disk")
 
 # define ResNet50 model
 ResNet50_model = ResNet50(weights='imagenet')
 
 dog_names = tuple(open('dog_names.txt', 'r'))
+
+# extract pre-trained face detector
+face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_alt.xml')
+
+print("Extracted face classifier")
 
 
 def path_to_tensor(img_path):
@@ -51,10 +58,18 @@ def dog_detector(img_path):
     return (prediction <= 268) & (prediction >= 151)
 
 
-def improved_face_detector(img_path):
-    data = paths_to_tensor([img_path]).astype('float32') / 255.
-    data = data.reshape((1, -1))
-    return clf.predict(data)[0]
+# Memory error when trying to use SVM on non-GPU instance
+# def face_detector(img_path):
+#     data = paths_to_tensor([img_path]).astype('float32') / 255.
+#     data = data.reshape((1, -1))
+#     return clf.predict(data)[0]
+
+# Use OpenCV Face detector
+def face_detector(img_path):
+    img = cv2.imread(img_path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray)
+    return len(faces) > 0
 
 
 def get_breed(predicted_vector):
@@ -96,7 +111,7 @@ def classify_breed(img_path):
         formatted_message = breed[0]
     else:
         formatted_message = 'mixture of {} and {}'.format(breed[0], breed[1])
-    if improved_face_detector(img_path):
+    if face_detector(img_path):
         message = 'This face looks like a.... {}'.format(formatted_message)
     elif dog_detector(img_path):
         message = 'The dog breed is a.... {}'.format(formatted_message)
